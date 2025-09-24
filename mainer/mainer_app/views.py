12c1e6f08af0e15d4ddac8a354a7298f1ca9a6a8
Студент группы IS <is@sielom.local>
2025-09-24 11:24:59 +0500

    html2

--- mainer/mainer_app/views.py
+++ mainer/mainer_app/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def mainer_view(request):
-    return render(request, 'main.html')
+    text = "ладно"
+    values = ["lo", "rem", "pe"]
+    return render(request, 'main.html', {'my_note': text, 'is_draw_note': True, 'values': values})
